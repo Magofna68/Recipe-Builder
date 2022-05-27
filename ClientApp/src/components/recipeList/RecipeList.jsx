008d6f8f192ffa094b2904527c0d94453ca9e9d0
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-05-27 14:42:41 -0700

    add dummyData to seed mock recipe data through ticketlist

--- ClientApp/src/components/recipeList/RecipeList.jsx
+++ ClientApp/src/components/recipeList/RecipeList.jsx
@@ -9,6 +9,58 @@ import Row from 'react-bootstrap/Row';
 export default function RecipeList(props) {
   const { recipeList, onRecipeSelection } = props;
   
+  const dummyData = [
+    {
+      whenRecipeClicked: {onRecipeSelection},
+      id: 0,
+      title: "Mama's Homemade French Gravy",
+      instructions:
+      "1. Pick out your favorite sauce and mix as you go. 2. Place in bowl and microwave.",
+      ingredients:"Milk, Your secret ingredient, & Butter",
+      origin: "Greek",
+      mealType:"Breakfast",
+      rating:"4.5",
+      key:0
+    },
+    {
+      whenRecipeClicked: {onRecipeSelection},
+      id: 1,
+      title: "Mama's Homemade Hummus",
+      instructions:
+      "1. Pick out your favorite hummus.",
+      ingredients:"Hummus",
+      origin: "Comfort",
+      mealType:"Snack",
+      rating:"3.5",
+      key:1
+    },
+    {
+      whenRecipeClicked: {onRecipeSelection},
+      id: 2,
+      title: "Mama's Homemade Hamburgers",
+      instructions:
+      "1. Pick out your favorite burger 2. Put Mama's sauce on it.",
+      ingredients:"Burger & Mama Sauce",
+      origin: "Traditional American",
+      mealType: "Dinner",
+      rating:"5.0",
+      key:2
+    },
+    {
+      whenRecipeClicked: {onRecipeSelection},
+      id: 3,
+      title: "Mama's Homemade Fries",
+      instructions:
+      "1. Pick your Size 2. Get your Fries.",
+      ingredients:"Potatoe & Butter",
+      origin: "French",
+      mealType:"Snack",
+      rating:"4.5",
+      key:3
+    },
+
+  ]
+
   return (
     <>
       <h1>Recipe List</h1>
@@ -17,7 +69,8 @@ export default function RecipeList(props) {
         display: "flex",
       }}>
         <Row>
-      {recipeList.map((recipe) =>
+      {/* {recipeList.map((recipe) => */}
+      {dummyData.map((recipe) => 
         <Recipe
           //function being passed down through prop drilling from Home -- onRecipeSelection
           whenRecipeClicked={onRecipeSelection}
