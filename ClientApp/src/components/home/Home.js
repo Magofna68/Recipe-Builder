469a791d669bfe38a055a7dc52192bc3de286809
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-05-31 11:08:46 -0700

    add dummyData to state to render cards

--- ClientApp/src/components/home/Home.js
+++ ClientApp/src/components/home/Home.js
@@ -20,7 +20,7 @@ export class Home extends Component {
           title: "Mama's Homemade French Gravy",
           instructions:
           "1. Pick out your favorite sauce and mix as you go. 2. Place in bowl and microwave.",
-          ingredients:"Milk, Your secret ingredient, & Butter",
+          ingredients:["Milk", "Cream", "Butter"],
           origin: "Greek",
           mealType:"Breakfast",
           rating:"4.5",
@@ -74,65 +74,6 @@ export class Home extends Component {
       formVisibleOnPage: false,
     };
   }
-
-  // const dummyData = [
-  //   {
-  //     whenRecipeClicked: {onRecipeSelection},
-  //     id: 0,
-  //     title: "Mama's Homemade French Gravy",
-  //     instructions:
-  //     "1. Pick out your favorite sauce and mix as you go. 2. Place in bowl and microwave.",
-  //     ingredients:"Milk, Your secret ingredient, & Butter",
-  //     origin: "Greek",
-  //     mealType:"Breakfast",
-  //     rating:"4.5",
-  //     quote: "Gravy, Baby!",
-  //     img: "https://vegcharlotte.files.wordpress.com/2013/10/vegan-sausage-gravy-biscuits-2.jpg",
-  //     key:0
-  //   },
-  //   {
-  //     whenRecipeClicked: {onRecipeSelection},
-  //     id: 1,
-  //     title: "Mama's Homemade Hummus",
-  //     instructions:
-  //     "1. Pick out your favorite hummus.",
-  //     ingredients:"Hummus",
-  //     origin: "Comfort",
-  //     mealType:"Snack",
-  //     rating:"3.5",
-  //     quote: "This was my grandaddy's favorite",
-  //     img: "https://thegirlmusteat.files.wordpress.com/2013/03/img_0856.jpg",
-  //     key:1
-  //   },
-  //   {
-  //     whenRecipeClicked: {onRecipeSelection},
-  //     id: 2,
-  //     title: "Mama's Homemade Hamburgers",
-  //     instructions:
-  //     "1. Pick out your favorite burger 2. Put Mama's sauce on it.",
-  //     ingredients:"Burger & Mama Sauce",
-  //     origin: "Traditional American",
-  //     mealType: "Dinner",
-  //     rating:"5.0",
-  //     quote: "Nothing better than man made burgers.",
-  //     img: "https://nyceats.files.wordpress.com/2008/05/island-burger-005.jpg",
-  //     key:2
-  //   },
-  //   {
-  //     whenRecipeClicked: {onRecipeSelection},
-  //     id: 3,
-  //     title: "Mama's Homemade Fries",
-  //     instructions:
-  //     "1. Pick your Size 2. Get your Fries.",
-  //     ingredients:"Potatoe & Butter",
-  //     origin: "French",
-  //     mealType:"Snack",
-  //     rating:"4.5",
-  //     quote: "We make em, you eat em.",
-  //     img: "https://64.media.tumblr.com/tumblr_m0jxzwVtQn1qaiktvo1_1280.jpg",
-  //     key:3
-  //   },
-  // ]
 
   handleClick = () => {
     if (this.state.selectedRecipe != null) {
