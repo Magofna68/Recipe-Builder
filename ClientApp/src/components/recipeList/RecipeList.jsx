cbf13f732ea43f9ee67dfec875b0b98c52778693
Brandon Magofna <magofna68@Brandons-MacBook-Pro.local>
2022-05-31 10:16:17 -0700

    update recipeList to include dummyData array to allow proper testing of UI experience

--- ClientApp/src/components/recipeList/RecipeList.jsx
+++ ClientApp/src/components/recipeList/RecipeList.jsx
@@ -9,65 +9,64 @@ import Row from 'react-bootstrap/Row';
 export default function RecipeList(props) {
   const { recipeList, onRecipeSelection } = props;
   
-  const dummyData = [
-    {
-      whenRecipeClicked: {onRecipeSelection},
-      id: 0,
-      title: "Mama's Homemade French Gravy",
-      instructions:
-      "1. Pick out your favorite sauce and mix as you go. 2. Place in bowl and microwave.",
-      ingredients:"Milk, Your secret ingredient, & Butter",
-      origin: "Greek",
-      mealType:"Breakfast",
-      rating:"4.5",
-      quote: "Gravy, Baby!",
-      img: "https://vegcharlotte.files.wordpress.com/2013/10/vegan-sausage-gravy-biscuits-2.jpg",
-      key:0
-    },
-    {
-      whenRecipeClicked: {onRecipeSelection},
-      id: 1,
-      title: "Mama's Homemade Hummus",
-      instructions:
-      "1. Pick out your favorite hummus.",
-      ingredients:"Hummus",
-      origin: "Comfort",
-      mealType:"Snack",
-      rating:"3.5",
-      quote: "This was my grandaddy's favorite",
-      img: "https://thegirlmusteat.files.wordpress.com/2013/03/img_0856.jpg",
-      key:1
-    },
-    {
-      whenRecipeClicked: {onRecipeSelection},
-      id: 2,
-      title: "Mama's Homemade Hamburgers",
-      instructions:
-      "1. Pick out your favorite burger 2. Put Mama's sauce on it.",
-      ingredients:"Burger & Mama Sauce",
-      origin: "Traditional American",
-      mealType: "Dinner",
-      rating:"5.0",
-      quote: "Nothing better than man made burgers.",
-      img: "https://nyceats.files.wordpress.com/2008/05/island-burger-005.jpg",
-      key:2
-    },
-    {
-      whenRecipeClicked: {onRecipeSelection},
-      id: 3,
-      title: "Mama's Homemade Fries",
-      instructions:
-      "1. Pick your Size 2. Get your Fries.",
-      ingredients:"Potatoe & Butter",
-      origin: "French",
-      mealType:"Snack",
-      rating:"4.5",
-      quote: "We make em, you eat em.",
-      img: "https://64.media.tumblr.com/tumblr_m0jxzwVtQn1qaiktvo1_1280.jpg",
-      key:3
-    },
-
-  ]
+  // const dummyData = [
+  //   {
+  //     whenRecipeClicked: {onRecipeSelection},
+  //     id: 0,
+  //     title: "Mama's Homemade French Gravy",
+  //     instructions:
+  //     "1. Pick out your favorite sauce and mix as you go. 2. Place in bowl and microwave.",
+  //     ingredients:"Milk, Your secret ingredient, & Butter",
+  //     origin: "Greek",
+  //     mealType:"Breakfast",
+  //     rating:"4.5",
+  //     quote: "Gravy, Baby!",
+  //     img: "https://vegcharlotte.files.wordpress.com/2013/10/vegan-sausage-gravy-biscuits-2.jpg",
+  //     key:0
+  //   },
+  //   {
+  //     whenRecipeClicked: {onRecipeSelection},
+  //     id: 1,
+  //     title: "Mama's Homemade Hummus",
+  //     instructions:
+  //     "1. Pick out your favorite hummus.",
+  //     ingredients:"Hummus",
+  //     origin: "Comfort",
+  //     mealType:"Snack",
+  //     rating:"3.5",
+  //     quote: "This was my grandaddy's favorite",
+  //     img: "https://thegirlmusteat.files.wordpress.com/2013/03/img_0856.jpg",
+  //     key:1
+  //   },
+  //   {
+  //     whenRecipeClicked: {onRecipeSelection},
+  //     id: 2,
+  //     title: "Mama's Homemade Hamburgers",
+  //     instructions:
+  //     "1. Pick out your favorite burger 2. Put Mama's sauce on it.",
+  //     ingredients:"Burger & Mama Sauce",
+  //     origin: "Traditional American",
+  //     mealType: "Dinner",
+  //     rating:"5.0",
+  //     quote: "Nothing better than man made burgers.",
+  //     img: "https://nyceats.files.wordpress.com/2008/05/island-burger-005.jpg",
+  //     key:2
+  //   },
+  //   {
+  //     whenRecipeClicked: {onRecipeSelection},
+  //     id: 3,
+  //     title: "Mama's Homemade Fries",
+  //     instructions:
+  //     "1. Pick your Size 2. Get your Fries.",
+  //     ingredients:"Potatoe & Butter",
+  //     origin: "French",
+  //     mealType:"Snack",
+  //     rating:"4.5",
+  //     quote: "We make em, you eat em.",
+  //     img: "https://64.media.tumblr.com/tumblr_m0jxzwVtQn1qaiktvo1_1280.jpg",
+  //     key:3
+  //   },
+  // ]
 
   return (
     <>
@@ -82,8 +81,8 @@ export default function RecipeList(props) {
         <Row styles={{
           display: "flex",
         }}>
-      {/* {recipeList.map((recipe) => */}
-      {dummyData.map((recipe) => 
+          {/* {{dummyData}.map((recipe) =>  */}
+      {recipeList.map((recipe) =>
         <Recipe
           //function being passed down through prop drilling from Home -- onRecipeSelection
           whenRecipeClicked={onRecipeSelection}
